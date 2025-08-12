39f87251011c086a1cd8b59b7f18e6bf1675809b
GucheaLeon <leonguchealezama@gmail.com>
2025-08-12 13:42:28 -0300

    Cambios en el registro

--- compania_naviera/compania_naviera/models.py
+++ compania_naviera/compania_naviera/models.py
@@ -8,28 +8,7 @@ class UsuarioPersonalizado(AbstractUser):
     email = models.EmailField(unique=True)
     telefono = models.CharField(max_length=20, blank=True)
     pais = models.CharField(max_length=50, blank=True)
-    metodo_pago = models.CharField(
-        max_length=30,
-        choices=[
-            ('tarjeta', 'Tarjeta de crédito/débito'),
-            ('paypal', 'PayPal'),
-            ('transferencia', 'Transferencia bancaria'),
-            ('mercadopago', 'Mercado Pago'),  
-        ],
-        blank=True
-    )
-
-    # Evitar conflicto con auth.User
-    groups = models.ManyToManyField(
-        Group,
-        related_name="usuario_personalizado_set",
-        blank=True
-    )
-    user_permissions = models.ManyToManyField(
-        Permission,
-        related_name="usuario_personalizado_permissions_set",
-        blank=True
-    )
+    
 
     def __str__(self):
         return self.username
