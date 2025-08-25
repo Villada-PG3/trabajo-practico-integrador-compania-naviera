0883b35fc2f72b239bd16e18e1e130345d5884e4
GucheaLeon <leonguchealezama@gmail.com>
2025-08-25 08:35:01 -0300

    agregado de base

--- compania_naviera/compania_naviera/forms.py
+++ compania_naviera/compania_naviera/forms.py
@@ -12,6 +12,15 @@ class FormularioRegistroPersonalizado(UserCreationForm):
             'pais', 'password1', 'password2'
         ]
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Asignar clase y placeholder a todos los campos
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({
+                "class": "form-control p-2 rounded-lg bg-white/10 text-white border border-white/30 focus:outline-none focus:ring-2 focus:ring-orange-400 w-full",
+                "placeholder": field.label
+            })
+
     def clean_email(self):
         email = self.cleaned_data.get('email')
         if UsuarioPersonalizado.objects.filter(email=email).exists():
