929bcbd3b61491e465162b8045cbf59fda6c303e
lorenzocirrincione <lolocirrin10@gmail.com>
2025-09-29 12:02:14 -0300

    a

--- compania_naviera/Config/urls.py
+++ compania_naviera/Config/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('contacto/', views.contacto_view, name='contacto'),
     path('contacto_log/', views.contacto_log_view, name='contacto_log'),
     path('destinos/', views.destinos_view, name='destinos'),
+    path("destinos/<int:pk>/", views.destino_detail_view, name="destino_detail"),
 
     path('mis-reservas/', views.mis_reservas_view, name='mis_reservas'),
     path('reservas/nueva/', views.crear_reserva_view, name='crear_reserva'),
