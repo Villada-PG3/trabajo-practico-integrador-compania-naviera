1c4eb8845d82b39bb4aae9924e1973c799c74785
Juan Pablo Rolo <juanpablorolo2007@gmail.com>
2025-09-29 14:41:33 +0000

    Commit menu_user

--- compania_naviera/Config/urls.py
+++ compania_naviera/Config/urls.py
@@ -2,32 +2,33 @@ from django.contrib import admin
 from django.urls import path, include
 from compania_naviera import views
 from django.contrib.auth import views as auth_views
-from django.urls import path
-from django.conf.urls.i18n import i18n_patterns
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('contacto/', views.contacto_view, name='contacto'),
-    path('cruceros/', views.cruceros_view, name='cruceros'),
-    path("cruceros/<int:pk>/", views.navio_detail_view, name="navio_detail"),
 
-    
-    path('ofertas/', views.ofertas_view, name='ofertas'),
-    path('login/', views.login_view, name='login'),
-    path('menu/', views.menu_user, name='menu_user'),
-    path('registro/', views.RegistroUsuario.as_view(), name='registro'),
     path('', views.main_view, name='home'),
+
+    path('login/', views.login_view, name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
-    
+    path('registro/', views.RegistroUsuario.as_view(), name='registro'),
+
+    path('menu/', views.menu_user, name='menu_user'),
+
     path('editar-perfil/', views.editar_perfil, name='editar_perfil'),
-    path('contacto_log/', views.contacto_log_view, name='contacto_log'),
     path('cambiar_contrasenia/', views.cambiar_contrasenia, name='cambiar_contrasenia'),
+
+    path('contacto/', views.contacto_view, name='contacto'),
+    path('contacto_log/', views.contacto_log_view, name='contacto_log'),
     path('destinos/', views.destinos_view, name='destinos'),
-    path("destinos/<int:pk>/", views.destino_detail_view, name="destino_detail"),
 
-   
-    path('cliente/nuevo/', views.crear_cliente_view, name='crear_cliente'),
-    path("mis-reservas/", views.mis_reservas_view, name="mis_reservas"),
-    path("reservas/nueva/", views.crear_reserva_view, name="crear_reserva"),
+    path('mis-reservas/', views.mis_reservas_view, name='mis_reservas'),
+    path('reservas/nueva/', views.crear_reserva_view, name='crear_reserva'),
 
+    # Cruceros + detalle
+    path('cruceros/', views.cruceros_view, name='cruceros'),
+    path('cruceros/<int:pk>/', views.navio_detail_view, name='navio_detail'),
+
+    # Ofertas (usada por base.html)
+    path('ofertas/', views.ofertas_view, name='ofertas'),
 ]
